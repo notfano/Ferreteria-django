e32a816459faeed84fbe191aeb030e4b40fec539
Stefano Rosati <98420047+notfano@users.noreply.github.com>
2024-10-24 00:50:16 -0300

    a little bit of everything

--- ferreteriadjango/ferreteriacore/admin.py
+++ ferreteriadjango/ferreteriacore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Producto
 # Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nombre', 'precio', 'disponible')  # Campos a mostrar en la lista
+    search_fields = ('nombre', 'codigo')  # Campos que se pueden buscar
+
+admin.site.register(Producto, ProductoAdmin)
--- ferreteriadjango/ferreteriacore/views.py
+++ ferreteriadjango/ferreteriacore/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pagina_principal(request):
+    return render(request, 'home.html')  # Reemplaza con el nombre de tu archivo HTML
