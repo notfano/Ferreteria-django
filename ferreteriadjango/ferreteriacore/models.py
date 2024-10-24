fc126d07c61a30b7f7e53e51e59590d8109e805b
Stefano Rosati <98420047+notfano@users.noreply.github.com>
2024-10-24 00:29:57 -0300

    fix

--- ferreteriadjango/ferreteriacore/models.py
+++ ferreteriadjango/ferreteriacore/models.py
@@ -6,7 +6,7 @@ class Producto(models.Model):
     descripcion = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     disponible = models.BooleanField(default=True)
-    codigo = models.CharField(max_length=100)\
+    codigo = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.nombre} ({self.codigo})"
